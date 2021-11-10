94ac7ad69b9db69f9dfb9f2839cc1525fd6e5f5b
hayoung kim <hayoung.kim@seavantage.com>
2021-11-11 01:13:55 +0900

    [feat] 기존 코드 upload

---
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example.querydsl;
+
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인 (hello.getId())
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+}
